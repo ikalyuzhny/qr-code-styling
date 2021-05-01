600999668bd1feb847b344f5bee4d947166bb339
ikalyzhny <illia.kaliuzhnyi@freshcodeit.net>
2021-05-01 10:14:47 +0300

    feat: build for new feature

--- lib/core/QRCodeStyling.d.ts
+++ lib/core/QRCodeStyling.d.ts
@@ -2,12 +2,14 @@
 import QRCanvas from "./QRCanvas";
 import { Options, RequiredOptions } from "./QROptions";
 import { QRCode } from "../types";
-import { PngConfig } from "canvas";
+import { Canvas, PngConfig } from "canvas";
 export default class QRCodeStyling {
     _options: RequiredOptions;
     _canvas?: QRCanvas;
     _qr?: QRCode;
     constructor(options?: Partial<Options>);
     draw(options?: Partial<Options>): Promise<QRCodeStyling>;
+    getCanvas(): Canvas | undefined;
     toPNGBuffer(options?: PngConfig): Buffer | undefined;
+    toSVGBuffer(): Buffer | undefined;
 }
